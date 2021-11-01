9cc813b38a37b1aff7d06db28f371c6e609656a3
Yessine Ben Hlima <87872940+BHYessine@users.noreply.github.com>
2021-11-01 15:40:12 +0100

    Update editReducer.js

--- src/Redux/Reducers/editReducer.js
+++ src/Redux/Reducers/editReducer.js
@@ -1,24 +1,23 @@
 import { EDIT_TASK } from "../Constants/types";
 import { data } from "../../Components/data";
 
-const initialState = {data};
+const initialState = data;
 
-export const editTask = (state = initialState, action) => {
-    switch (action.type) {
-        case EDIT_TASK:
-            return {
-                data : state.data.map( (item) => {
-                            if (item.id === action.payload.id) {
-                                return {
-                                    ...item,
-                                    description: action.payload.task,
-                                    isDone: false
-                                }
-                            }
-                            return state
-                        })
-            }    
-        default:
-            return state
-    }
-}
+export const editReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case EDIT_TASK:
+      const index = state.todos.findIndex(
+        (todo) => todo.id === action.payload.id
+      );
+      const newArray = [...state.todos];
+      newArray[index].description = action.payload.description;
+      newArray[index].isDone = false;
+      return {
+        ...state,
+        todos: newArray
+      };
+
+    default:
+      return state;
+  }
+};
