5d1c32be752925b480df08b3ded50f1bb519d35b
Yessine Ben Hlima <87872940+BHYessine@users.noreply.github.com>
2021-11-01 15:56:57 +0100

    Update store.js

--- src/Redux/store/store.js
+++ src/Redux/store/store.js
@@ -2,13 +2,13 @@ import { createStore, combineReducers } from "@reduxjs/toolkit";
 import { addReducer } from "../Reducers/addReducer";
 import { doneReducer } from "../Reducers/doneReducer"
 import { editReducer } from "../Reducers/editReducer"
-//import { removeTask } from "../Reducers/deleteReducer"
+import { removeReducer } from "../Reducers/removeReducer"
 
  const reducer = combineReducers({   
     add: addReducer,
     done: doneReducer,
     edit: editReducer,
-    //remove: removeTask
+    remove: removeReducer,
  })
 
 const store = createStore(reducer);
