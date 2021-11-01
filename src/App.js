f3cdea7c3bbe9a03e8533e6b901a5a9c8f625a9e
Yessine Ben Hlima <87872940+BHYessine@users.noreply.github.com>
2021-11-01 11:19:35 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from "react";
-import { Provider} from 'react-redux'
 import ListTask from "./Components/ListTask";
 import AddTask from "./Components/AddTask";
-import store from "./Redux/store/store"
 import "./App.css";
 
 function App() {
   
   return (
-    <Provider store={store}>
+    
       <div className="appContainer">
         <p className="title"> ToDo Application </p>
         <AddTask />        
         <ListTask />
       </div>
-    </Provider>
+    
   );
 }
 
