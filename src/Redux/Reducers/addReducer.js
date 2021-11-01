6b7dce056b1df0257428868fb96ffd7723570b8d
Yessine Ben Hlima <87872940+BHYessine@users.noreply.github.com>
2021-11-01 15:39:12 +0100

    Update addReducer.js

--- src/Redux/Reducers/addReducer.js
+++ src/Redux/Reducers/addReducer.js
@@ -5,14 +5,17 @@ import { data } from "../../Components/data";
 const initialState = data;
 
 export const addReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case ADD_TASK:
-            return {
-                ...state,                       
-                todos: state.todos.concat(action.payload)                  
-            }    
-        default:
-            return state
-    }
-}
-// export default addReducer;
+  switch (action.type) {
+    case ADD_TASK:        
+        const newArray = [...state.todos];
+        newArray.isDone = false;
+        newArray.description = action.payload.description;
+        newArray.id = action.payload.id;
+      return {
+        ...state,
+        todos: newArray,        
+      };
+    default:
+      return state;
+  }
+};
